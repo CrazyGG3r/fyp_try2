30c21e35acc30ce54153efc1c96da0e01944565d
Shaheer Ul Islam <shaheerislam.islam179@gmail.com>
2024-10-16 13:34:33 +0500

    tried to tidy up dummy

--- Dummy_Agent/dummy.py
+++ Dummy_Agent/dummy.py
@@ -1,16 +1,27 @@
 import socket
 import time
 
-def send_key(key):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(('localhost', 9999))
-        s.sendall(key.encode('utf-8'))
+
+
+class dummy:
+    def __init__(self,ipee = 'localhost',port = 9999):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ip = ipee
+        self.port = port
+        self.addresss = (self.ip,self.port)
+        self.server.connect(self.addresss)
+    
+    def send_key(self,key):
+        self.server.sendall(key.encode('utf-8'))
         print(f"Sent key: {key}")
 
-if __name__ == "__main__":
-    time.sleep(1)  # Wait for the server to start
-    # Send arrow key inputs as a demonstration
-    keys = ['UP', 'DOWN', 'LEFT', 'RIGHT']
-    for key in keys:
-        send_key(key)
-        time.sleep(1)  # Wait a second between each key press
+
+time.sleep(1)  # Wait for the server to start
+# Send arrow key inputs as a demonstration
+keys = ['UP', 'DOWN', 'LEFT', 'RIGHT']
+run = True
+agent1 = dummy()
+key = "hello"
+while run:
+    agent1.send_key(key)
+    time.sleep(1)  # Wait a second between each key press
