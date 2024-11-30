72d39f40bc39b5be6fceb04b1af031573fdba70a
Shaheer Ul Islam <shaheerislam.islam179@gmail.com>
2024-11-30 22:45:33 +0500

    ball done, finetune physics now and then add goals

--- Environment/Scripts/Hexapod_1.gd
+++ Environment/Scripts/Hexapod_1.gd
@@ -1,30 +1,37 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 350.0
 const angular_speed = deg_to_rad(20)
 var movement = Vector2(0,0)
 
 var fake_delta = null
 func _physics_process(delta):
-	#if Input.is_action_just_pressed("BACKWARDS"):
-		#movement = Vector2(sin(rotation),cos(rotation)) * SPEED
-		#position += movement * delta
-	#if Input.is_action_just_pressed("FORWARDS"):
-		#movement = Vector2( sin(rotation),-cos(rotation)) * SPEED
-		#position += movement * delta
-	#if Input.is_action_just_pressed("RIGHT"):
-		#movement = Vector2( cos(rotation),sin(rotation)) * SPEED
-		#position += movement * delta
-	#if Input.is_action_just_pressed("LEFT"):
-		#movement = Vector2( -cos(rotation),sin(rotation)) * SPEED
-		#position += movement * delta
-	#if Input.is_action_just_pressed("RIGHT_ROTATE"):
-		#rotation += angular_speed 
-	#if Input.is_action_just_pressed("LEFT_ROTATE"):
-		#rotation -= angular_speed 
-		fake_delta = delta
+	if Input.is_action_just_pressed("BACKWARDS"):
+		movement = Vector2(sin(rotation),cos(rotation)) * SPEED
+		position += movement * delta
+		#move_and_collide(movement)
+	if Input.is_action_just_pressed("FORWARDS"):
+		movement = Vector2( sin(rotation),-cos(rotation)) * SPEED
+		position += movement * delta
+		#move_and_collide(movement)
+	if Input.is_action_just_pressed("RIGHT"):
+		movement = Vector2( cos(rotation),sin(rotation)) * SPEED
+		position += movement * delta
+		#move_and_collide(movement)
+	if Input.is_action_just_pressed("LEFT"):
+		movement = Vector2( -cos(rotation),sin(rotation)) * SPEED
+		position += movement * delta
+		#move_and_collide(movement)
+	if Input.is_action_just_pressed("RIGHT_ROTATE"):
+		rotation += angular_speed 
+		#move_and_collide(movement)
 		
+	if Input.is_action_just_pressed("LEFT_ROTATE"):
+		rotation -= angular_speed 
+		fake_delta = delta
+		#move_and_collide(movement)
+	move_and_slide()
 
 
 func _on_environment_handel_action(action_str):
@@ -44,6 +51,6 @@ func _on_environment_handel_action(action_str):
 		rotation += angular_speed 
 	if action_str == "L2":
 		rotation -= angular_speed
-		
+	
 	
 	print(action_str)
