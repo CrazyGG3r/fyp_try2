fd51381c208122335670d3e48a915071f4989993
Shaheer Ul Islam <shaheerislam.islam179@gmail.com>
2024-11-30 23:32:03 +0500

    fixed more movements bugs .

--- Environment/Scripts/Hexapod_1.gd
+++ Environment/Scripts/Hexapod_1.gd
@@ -8,28 +8,31 @@ var movement = Vector2(0,0)
 var fake_delta = null
 func _physics_process(delta):
 	if Input.is_action_just_pressed("BACKWARDS"):
-		movement = Vector2(sin(rotation),cos(rotation)) * SPEED
-		position -= movement * delta
+		movement = Vector2(-sin(rotation),cos(rotation)) * SPEED
+		#position -= movement * delta
 		move_and_collide(movement*delta)
+		
 	if Input.is_action_just_pressed("FORWARDS"):
 		movement = Vector2( sin(rotation),-cos(rotation)) * SPEED
-		position += movement * delta
+		#position += movement * delta
 		move_and_collide(movement*delta)
 		
 	if Input.is_action_just_pressed("RIGHT"):
 		movement = Vector2( cos(rotation),sin(rotation)) * SPEED
-		position += movement * delta
+		#position += movement * delta
 		move_and_collide(movement*delta)
 		
 	if Input.is_action_just_pressed("LEFT"):
 		movement = Vector2( -cos(rotation),sin(rotation)) * SPEED
-		position += movement * delta
+		#position += movement * delta
 		move_and_collide(movement*delta)
 		
 	if Input.is_action_just_pressed("RIGHT_ROTATE"):
 		rotation += angular_speed 
+		#move_and_collide(movement*fake_delta)
 	if Input.is_action_just_pressed("LEFT_ROTATE"):
 		rotation -= angular_speed 
+		#move_and_collide(movement*fake_delta)
 		
 	fake_delta = delta
 		
@@ -38,26 +41,26 @@ func _physics_process(delta):
 func _on_environment_handel_action(action_str):
 	if action_str == "FK":
 		movement = Vector2( sin(rotation),-cos(rotation)) * SPEED
-		position += movement * fake_delta
+		#position += movement * fake_delta
 		move_and_collide(movement*fake_delta)
 	if action_str == "BK":
 		movement = Vector2(sin(rotation),cos(rotation)) * SPEED
-		position -= movement * fake_delta
+		#position -= movement * fake_delta
 		move_and_collide(movement*fake_delta)
 	if action_str == "RT":
 		movement = Vector2( cos(rotation),sin(rotation)) * SPEED
-		position += movement * fake_delta
+		#position += movement * fake_delta
 		move_and_collide(movement*fake_delta)
 	if action_str == "LT":
 		movement = Vector2( -cos(rotation),sin(rotation)) * SPEED
-		position += movement * fake_delta
+		#position += movement * fake_delta
 		move_and_collide(movement*fake_delta)
 	if action_str == "R2":
 		rotation += angular_speed 
-		move_and_collide(movement*fake_delta)
+		#move_and_collide(movement*fake_delta)
 	if action_str == "L2":
 		rotation -= angular_speed
-		move_and_collide(movement*fake_delta)
+		#move_and_collide(movement*fake_delta)
 	
 	
 	print(action_str)
