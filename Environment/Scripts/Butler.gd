556fae1680732634445b6e9ffc20012eba49e509
Shaheer Ul Islam <shaheerislam.islam179@gmail.com>
2024-12-04 14:54:23 +0500

    5 state vectors added and working

--- Environment/Scripts/Butler.gd
+++ Environment/Scripts/Butler.gd
@@ -8,7 +8,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 var state_vector = []
 var states = 10
-var flag_connected = 0
+var flag_connected = 1
 
 signal send_state_vector(state_vector)
 
@@ -21,16 +21,28 @@ func _ready():
 
 @onready var hexapodd = $"../hexapod"
 @onready var ball = $"../Ball"
+@onready var wall_top = $"../top/coll"
+@onready var wall_bot =$"../bottom/coll"
+@onready var wall_right = $"../right/coll"
+@onready var wall_left =$"../left/coll"
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _process(delta):
 	if flag_connected ==1:
 		
+		# Hexapod from ball
 		state_vector[0] = calculate_polar_coordinate(hexapodd)
-		print(state_vector)
-		print(flag_connected)
+		# wall_top from ball
+		state_vector[1] = Vector2(calculate_y_distance(wall_top)-110,deg_to_rad(90))
+		#wall_bottom from ball
+		state_vector[2] = Vector2(-(calculate_y_distance(wall_bot)-110),deg_to_rad(270))
+		#wall_right from ball
+		state_vector[3] = Vector2(-(calculate_x_distance(wall_right)-110),deg_to_rad(0))
+		#wall_left from ball
+		state_vector[4] = Vector2(calculate_x_distance(wall_left)-110,deg_to_rad(180))
+		
+		
 		send_state_vector.emit(state_vector)
-	
 	
 func calculate_polar_coordinate(entity):
 	var r = ball.balls[0].global_position.distance_to(entity.global_position)
@@ -40,6 +52,7 @@ func calculate_polar_coordinate(entity):
 func calculate_y_distance(static_entity):
 	var d = ball.balls[0].global_position.y - static_entity.global_position.y
 	return d	
+	
 func calculate_x_distance(static_entity):
 	var d = ball.balls[0].global_position.x - static_entity.global_position.x
 	return d	
